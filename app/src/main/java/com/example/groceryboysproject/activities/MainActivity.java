779774fd8eb8f3148b8141a3d5bcfbbb140d4b24
Ian Nelson <Ian.nelson@cwu.edu>
2024-11-29 22:04:16 -0800

    src changes

--- app/src/main/java/com/example/groceryboysproject/activities/MainActivity.java
+++ app/src/main/java/com/example/groceryboysproject/activities/MainActivity.java
@@ -11,7 +11,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.groceryboysproject.R;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,10 +24,22 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // quick code to switch to graph :)
+        Button buttonSwitch = findViewById(R.id.buttonSwitch);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        buttonSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent to switch to SecondActivity
+                Intent intent = new Intent(MainActivity.this, GraphActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
